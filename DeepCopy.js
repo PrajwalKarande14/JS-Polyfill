d95c42e237aa2c6ff1a076a3185fc530f2ddc2d9
PrajwalKarande14 <prajwal_rajendrakarande@epam.com>
2026-02-04 15:59:11 +0530

    Throttling Done

--- DeepCopy.js
+++ DeepCopy.js
@@ -1,23 +1,30 @@
 const obj = {
     a: 1,
     b:[1,2,3,{name:"prajwal"}],
-    c:null
 }
 
-const DeepCopy = function(obj){
+obj.c = obj //creating a circular reference
+
+const DeepCopy = function(obj,map){
+
     if(typeof obj !== "object" || obj===null){
         return obj
+    }
+    if(map.has(obj)){
+        return map.get(obj)
     }
     let res
     if(Array.isArray(obj)){
         res = []
+        map.set(obj,res)
         for(let i in obj){
-            res.push(DeepCopy(obj[i]))
+            res.push(DeepCopy(obj[i],map))
         }
     }else{
         res = {}
+        map.set(obj,res)
         for(let i in obj){
-            res[i] = DeepCopy(obj[i])
+            res[i] = DeepCopy(obj[i],map)
         }
         
     }
@@ -25,7 +32,8 @@ const DeepCopy = function(obj){
 
 }
 
-const res = DeepCopy(obj)
+const map = new WeakMap()
+const res = DeepCopy(obj,map)
 
 
 res.b.push("rvlknev")
--- Throttling.js
+++ Throttling.js
@@ -1,1 +1,28 @@
-const throttle = (func, limit) => {
+const throttle = (fn, limit,mode = {leading : true, trailing:true}) => {
+    let lastCall = 0
+    let lastFun
+    return function(...args){
+        const now = Date.now()
+        if(lastCall && mode.leading === false){
+            lastCall = now
+        }
+
+        const remaining = limit - (now - lastCall)
+
+        if(remaining<0){
+            if(lastFun){
+                clearTimeout(lastFun)
+                lastFun = null
+            }
+            lastCall = now
+            fn.apply(this,args)
+        }else if(!mode.trailing && lastFun){
+            lastFun = setTimeout(() => {
+                lastCall = mode.leading === false ? 0 : now
+                lastFun = null
+                fn.apply(this,args)
+            }, remaining);
+        }
+
+    }
+}   
