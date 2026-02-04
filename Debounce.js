28ec9a8df00fdab57685daba903f059229d7a5b9
PrajwalKarande14 <prajwal_rajendrakarande@epam.com>
2026-02-04 11:33:40 +0530

    Debounce

--- Debounce.js
+++ Debounce.js
@@ -1,12 +1,20 @@
 const debounce = function(fn,wait,immediate=false){
     let timeout;
     return function(...args){
-        clearTimeout(timeout)
-        timeout = setTimeout(()=>fn.apply(this,args),wait)
+        if(timeout) clearTimeout(timeout)
+        if(immediate){
+            const callNow = !timeout
+            timeout = setTimeout(()=>{timeout = null},wait)
+            if(callNow){
+                fn.apply(this,args)
+            }
+        }else{
+            timeout = setTimeout(()=>fn.apply(this,args),wait)
+        }
     }
 }
 
-const log = debounce(console.log, 100);
+const log = debounce(console.log, 100,true);
 log('Hello'); // cancelled
 log('Hello'); // cancelled
 log('Hello'); // Logged at t=100ms
