20ea140e10017678cc5b9f5e889b3e42a93bef9a
PrajwalKarande14 <prajwal_rajendrakarande@epam.com>
2026-02-05 11:06:36 +0530

    Async Await with Generator function

--- GeneratorAsyncAwait.js
+++ GeneratorAsyncAwait.js
@@ -1,1 +1,40 @@
-// loading.....
+const asyncAwait = function* (){
+    console.log("start of Generator")
+
+    yield  new Promise((res,rej)=>{
+        setTimeout(()=>{
+            res("Promise 1")
+        },1000)
+    })
+
+    console.log("middle of Generator")
+
+    yield  new Promise((res,rej)=>{
+        setTimeout(()=>{
+            res("Promise 2")
+        },3000)
+    })
+
+    console.log("End of Generator")
+}
+
+
+const runner = function(genFn){
+    const res = genFn.next()
+    if(res.done){
+        return
+    }
+    Promise.resolve(res.value).then((val)=>{
+        console.log(val)
+        runner(genFn)
+    }).catch((err)=>{
+        console.error(err)
+        runner(genFn)
+    })
+    console.log("runner complete")
+    
+}
+
+
+const genFn = asyncAwait()
+runner(genFn)
